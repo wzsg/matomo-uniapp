4a5f0116879a27747f85940a6e13f78c02027c84
william <william@juyou88.com>
2024-03-11 11:50:44 +0800

    Add Matomo tracking plugin for UniApp
    Added a Matomo tracking plugin for UniApp with an accompanying README file and its own package.json. Also created the necessary npm token authentication file and updated the .gitignore.

--- README.md
+++ README.md
@@ -1,1 +1,42 @@
-# matomo-uniapp
+
+## Matomo Plugin for UniApp
+
+This [Matomo](https://www.matomo.org) plugin is designed for [UniApp](https://uniapp.dcloud.io).
+
+### Installation
+
+```bash
+npm install matomo-uniapp
+```
+
+### Usage
+
+```javascript
+import Matomo from 'matomo-uniapp';
+
+const matomo = new Matomo({
+  url: 'https://your-matomo-instance.com',
+  siteId: 1,
+});
+
+matomo.setUserId('example-user-id');
+```
+
+### API
+
+#### `new Matomo(options)`
+
+- `options` (Object):
+  - `url` (String): The URL of your Matomo instance.
+  - `siteId` (Number): The ID of the site you want to track.
+
+#### `matomo.setUserId(userId)`
+- `userId` (String): Defines the User ID for this request. The User ID is any non-empty unique string identifying the user (such as an email address or a username).
+
+### License
+
+[MIT License](https://opensource.org/licenses/MIT)
+
+### Keywords
+
+[matomo](https://www.npmjs.com/search?q=matomo), [uniapp](https://www.npmjs.com/search?q=uniapp)
---
+++ plugin.js
@@ -0,0 +1,110 @@
+import sha1 from 'js-sha1'
+
+const Matomo = {
+    install(Vue, options) {
+        const pluginName = "MatomoUniApp";
+        const pluginVersion = "v1.0.0";
+        if (uni===undefined || !uni) {
+            console.log(`${pluginName} ${pluginVersion} install fail - Not in UniApp`)
+            return
+        }
+
+        this.matomoUrl = options.url;
+        this.matomoSiteId = options.siteId;
+
+        this.navigateTo = uni.navigateTo;
+        this.redirectTo = uni.redirectTo;
+        this.reLaunch = uni.reLaunch;
+        this.switchTab = uni.switchTab;
+
+        const _this = this;
+        //唯一访客 ID，必须是 16 个字符的十六进制字符串
+        this.userId = null;
+        this.resolution = null;
+        uni.getSystemInfo({
+            success: function (res) {
+                console.log("getSystemInfo 2")
+                _this.userId = sha1(res.deviceId).substring(0, 16);
+                // 获取设备的分辨率
+                _this.resolution = res.windowWidth + 'x' + res.windowHeight;
+            }
+        });
+
+        this.uid = null;
+        Vue.prototype.$setUid = function (uid) {
+            _this.uid = uid;
+        }
+
+        this.trackPageView = (options) => {
+            //console.log("MatomoPlugin trackPageView", options)
+            const postData = {
+                idsite: this.matomoSiteId,
+                rec: 1,
+                action_name: options.actionName,
+                url: "app://uniapp"+options.pageUrl,
+                apiv: 1
+            }
+            postData.rand = Math.floor(Math.random() * 100000);
+            let now = new Date(); // 创建一个Date对象，表示当前时间
+            let hour = now.getHours(); // 获取当前小时
+            let minute = now.getMinutes(); // 获取当前分钟
+            let second = now.getSeconds(); // 获取当前秒数
+            postData.h = hour;
+            postData.m = minute;
+            postData.s = second;
+            if(this.userId!==null){
+                postData._id = this.userId;
+            }
+            if(this.resolution!==null){
+                postData.res = this.resolution;
+            }
+            if(this.uid!==null){
+                postData.uid = this.uid;
+            }
+            console.log(postData)
+            uni.request({
+                url: this.matomoUrl + "/matomo.php",
+                data: postData,
+                method: 'GET',
+                success: (res) => {
+
+                },
+                fail: (res) => {
+
+                }
+            });
+        }
+
+        uni.navigateTo = (options) => {
+            this.trackPageView({
+                actionName: "navigateTo",
+                pageUrl: options.url
+            });
+            this.navigateTo(options);
+        }
+        uni.redirectTo = (options) => {
+            this.trackPageView({
+                actionName: "redirectTo",
+                pageUrl: options.url
+            });
+            this.redirectTo(options);
+        }
+        uni.reLaunch = (options) => {
+            this.trackPageView({
+                actionName: "reLaunch",
+                pageUrl: options.url
+            });
+            this.reLaunch(options);
+        }
+        uni.switchTab = (options) => {
+            this.trackPageView({
+                actionName: "switchTab",
+                pageUrl: options.url
+            });
+            this.switchTab(options);
+        }
+        console.log(`${pluginName} ${pluginVersion} install success`)
+    }
+}
+
+export default Matomo;
